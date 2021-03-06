97fd7ee6f0acd7ec823ca0b9854c504d420b4dab
Thiago2436 <54513342+Thiago2436@users.noreply.github.com>
2021-03-05 21:43:07 -0300

    programa de IMC

---
+++ imc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link rel="stylesheet" href="estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Indice de massa corporal</title>
+</head><body></body>
+
+<?php
+
+        $peso = $_GET['peso'];
+        $altura = $_GET['altura'];
+
+        $conta1 = $altura*$altura;
+        $conta2 = $peso/$conta1;
+
+        $resultado = number_format($conta2);
+
+        if(isset($resultado) && $resultado != '0'){;	
+                echo "Seu IMC é: ".$resultado;               
+        }else{
+                echo '<h1>Por favor, utilize apenas numeros!</h1>';	
+        }    
+    ?>
+</html>
